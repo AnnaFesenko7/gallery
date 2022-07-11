95dc1cfda13b8186885bae624e95eef5695fcf34
AnnaFesenko7 <annafecenko7@gmail.com>
2022-07-11 11:56:43 +0200

    add images

---
+++ src/imp.js
@@ -0,0 +1,6 @@
+const img = {
+  1: './images/gallery/1.jpg',
+  2: './images/gallery/2.jpg',
+  3: './images/gallery/3.jpg',
+};
+export default img;
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,18 +1,20 @@
 import dataGalleryMarkup from '../data.json';
+import * as imgs from '../images/gallery/*.jpg';
+console.log(imgs);
 console.log(dataGalleryMarkup);
-const galleryContainer = document.querySelector('.page');
+const galleryContainer = document.querySelector('.gallery');
 const cardsMarkup = createGalleryCardMarkup();
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 function createGalleryCardMarkup() {
-	return dataGalleryMarkup
-		.map(({ description, src, title }) => {
-			return `
+  return dataGalleryMarkup
+    .map(({ description, src, title }, index) => {
+      return `
         <li class="galleryItem">
 					<div class="card">
 						<a
 							class="cardLink"
-							href=${src}
+							href=${imgs[index + 1]}
 							target="_blank"
 							data-title=${title}
 							data-description=${description}
@@ -22,11 +24,11 @@ function createGalleryCardMarkup() {
 								<p class="cardDescription">${description}</p>
 							</div>
 							<div class="cardHover"></div>
-							 <img class="cardImage" src=${src} alt=${title} /> 
+							 <img class="cardImage" src=${imgs[index + 1]} alt=${title} /> 
 						</a>
 					</div>
 				</li>
         `;
-		})
-		.join('');
+    })
+    .join('');
 }
